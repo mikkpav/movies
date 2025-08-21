67bf7754fed0f7ccfd0de40b2d6a27c7bd12c3b4
mikkpav <mikk.pavelson@gmail.com>
2025-08-21 23:05:19 +0300

    Take out the Favorites link for now.

--- src/LeftMenu.tsx
+++ src/LeftMenu.tsx
@@ -1,10 +1,10 @@
 import LeftMenuItem from './LeftMenuItem';
 
 export default function LeftMenu() {
+    // <LeftMenuItem to='/favorites'>Favorites</LeftMenuItem>
     return (
         <div className='flex flex-col flex-1 text-lg gap-2'>
             <LeftMenuItem to='/'>Popular</LeftMenuItem>
-            <LeftMenuItem to='/favorites'>Favorites</LeftMenuItem>
         </div>
     )
 }
