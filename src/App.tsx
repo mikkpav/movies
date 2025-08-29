9872c2296e2f98f3b7165725fcba315cf312dd5d
mikkpav <mikk.pavelson@gmail.com>
2025-08-29 08:46:56 +0300

    Make the mobile main view a column.

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,12 @@ import { FavoritesProvider } from './features/favorites/hooks/FavoritesContext';
 
 function App() {
     return (
-        <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-10 m-4 md:m-0">
             <header className="flex justify-center max-h-30 pt-6">
                 <img src={FilmIcon} className="object-contain max-w-20"></img>
                 <h1 className="text-center p-6 text-4xl font-bold">Movies</h1>
             </header>
-            <div className="flex flex-row gap-4">
+            <div className="flex flex-col md:flex-row gap-4">
                 <FavoritesProvider>
                     <LeftMenu />
                     <ContentArea>
